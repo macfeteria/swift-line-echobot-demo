df77d6406dff5bc8b1bf622264dac69a34591c77
Ter <macfeteria@gmail.com>
2017-03-15 01:59:31 +0700

    add dependency

---
+++ Package.swift
@@ -0,0 +1,11 @@
+import PackageDescription
+
+let package = Package(
+    name: "demo",
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6),
+        
+        .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0)
+    ]
+)
